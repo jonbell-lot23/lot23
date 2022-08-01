a679c08f2521a12bbaa876cd6721e0c29225585b
Jon Bell <github@lot23.com>
2022-08-01 22:04:53 +1200

    Initial lot23 changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,114 +9,57 @@ type Message = {
 };
 
 export default function Home() {
-  const [username, setUsername] = useState("");
-  const [chosenUsername, setChosenUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState<Array<Message>>([]);
-
-  useEffect(() => {
-    socketInitializer();
-  }, []);
-
-  const socketInitializer = async () => {
-    // We just call it because we don't need anything else out of it
-    await fetch("/api/socket");
-
-    socket = io();
-
-    socket.on("newIncomingMessage", (msg) => {
-      setMessages((currentMsg) => [
-        ...currentMsg,
-        { author: msg.author, message: msg.message },
-      ]);
-      console.log(messages);
-    });
-  };
-
-  const sendMessage = async () => {
-    socket.emit("createdMessage", { author: chosenUsername, message });
-    setMessages((currentMsg) => [
-      ...currentMsg,
-      { author: chosenUsername, message },
-    ]);
-    setMessage("");
-  };
-
-  const handleKeypress = (e) => {
-    //it triggers by pressing the enter key
-    if (e.keyCode === 13) {
-      if (message) {
-        sendMessage();
-      }
-    }
-  };
 
   return (
-    <div className="flex items-center p-4 mx-auto min-h-screen justify-center bg-purple-500">
-      <main className="gap-4 flex flex-col items-center justify-center w-full h-full">
-        {!chosenUsername ? (
-          <>
-            <h3 className="font-bold text-white text-xl">
-              How people should call you?
-            </h3>
-            <input
-              type="text"
-              placeholder="Identity..."
-              value={username}
-              className="p-3 rounded-md outline-none"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <button
-              onClick={() => {
-                setChosenUsername(username);
-              }}
-              className="bg-white rounded-md px-4 py-2 text-xl"
-            >
-              Go!
-            </button>
-          </>
-        ) : (
-          <>
-            <p className="font-bold text-white text-xl">
-              Your username: {username}
-            </p>
-            <div className="flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md ">
-              <div className="h-full last:border-b-0 overflow-y-scroll">
-                {messages.map((msg, i) => {
-                  return (
-                    <div
-                      className="w-full py-1 px-2 border-b border-gray-200"
-                      key={i}
-                    >
-                      {msg.author} : {msg.message}
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="border-t border-gray-300 w-full flex rounded-bl-md">
-                <input
-                  type="text"
-                  placeholder="New message..."
-                  value={message}
-                  className="outline-none py-2 px-2 rounded-bl-md flex-1"
-                  onChange={(e) => setMessage(e.target.value)}
-                  onKeyUp={handleKeypress}
-                />
-                <div className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all">
-                  <button
-                    className="group-hover:text-white px-3 h-full"
-                    onClick={() => {
-                      sendMessage();
-                    }}
-                  >
-                    Send
-                  </button>
-                </div>
-              </div>
+    
+    <section className="WorkSection WorkSection--fullBleed" className="m-16">
+      <div className="Container">
+        <div className="Grid">
+          <div className="Grid-cell u-md-width1of2">
+            <div className="WorkSection-content">
+              <h3>Hello! <br />I'm Jon Bell.</h3>
+              <p>
+                I'm a product designer and I love to write, code, draw, teach,
+                lead teams, and learn new things.
+              </p>
+
+              <p>
+                <a href="twitter.html">1. Here's some work I did at Twitter</a
+                ><br />
+                <a href="microsoft.html"
+                  >2. And here's what I did at Microsoft</a
+                ><br />
+                <a href="storytelling.html"
+                  >3. I love to write and give talks</a
+                >
+
+                <br /><br />
+                <b>Recent projects</b>
+              </p>
+
+              <p>
+                <a href="https://medium.com/near-future-field-notes"
+                  >1. Near Future Field Notes</a
+                ><br />
+
+                <a href="http://uxlaunchpad.com/designexplosions/"
+                  >2. Design Explosions</a
+                ><br />
+
+                <a
+                  href="https://medium.com/near-future-field-notes/8-beating-disinformation-2354f3c06261?source=collection_home---6------0-----------------------"
+                  >3. Beating Disinformation</a
+                >
+              </p>
+              <p>
+                My email is
+                <a href="mailto:jb@lot23.com">jb@lot23.com</a>. <br />Thanks for
+                stopping by!
+              </p>
             </div>
-          </>
-        )}
-      </main>
-    </div>
+          </div>
+        </div>
+      </div>
+    </section>  
   );
 }
