4d03625a1a192c0d8c1be352dc9f8764cc95a1e8
Jon Bell <github@lot23.com>
2022-08-07 10:26:15 +1200

    autoPlay fix

--- src/pages/microsoft.tsx
+++ src/pages/microsoft.tsx
@@ -164,8 +164,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </section>
-    {/* sucks at abuse divider */}
     {/*   
   <section class="WorkSection WorkSection--fullBleed" style="background-image: url('assets/suckAtAbuse.jpg');">
   
