3844950097ef165abb3fca4fd5a3d4a0e3123e15
Jon Bell <github@lot23.com>
2022-08-22 10:24:55 +1200

    Fixing moble

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (    
     <div className="w-56 mx-auto mt-8 sm:w-80 sm:px-0">    
       <Header />
-      <Footer />
+      <Body />
     </div>
     
   );
@@ -25,7 +25,7 @@ export function Header() {
     <div className="flex flex-wrap space-x-3">
       <Link href="/talks"><a><img src="/talks.png" className="header" /></a></Link>
       <Link href="/writing"><a><img src="/writing.png" className="header" onMouseOver={e => e.currentTarget.src = "writing_highlight.png"} onMouseOut={e => e.currentTarget.src = "writing.png"}/></a></Link>
-      <Link href="/projects"><a><img src="/projects_highlight.png" className="header" onMouseOver={e => e.currentTarget.src = "projects_highlight.png"} onMouseOut={e => e.currentTarget.src = "projects.png"}/></a></Link>
+      <img src="/projects_highlight.png" className="header" onMouseOver={e => e.currentTarget.src = "projects_highlight.png"} onMouseOut={e => e.currentTarget.src = "projects.png"}/>
       <Link href="/work"><a><img src="/work.png" className="header" onMouseOver={e => e.currentTarget.src = "work_highlight.png"} onMouseOut={e => e.currentTarget.src = "work.png"}/></a></Link>
     </div>
   )
@@ -33,11 +33,11 @@ export function Header() {
 
 
 
-export function Footer() {
+export function Body() {
   return (
     <div className="grid mt-12 place-items-center">
       <div>
-        <img src="/hi.png" />
+        
       </div>
     </div>
     )
--- src/pages/writing.tsx
+++ src/pages/writing.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (    
     <div className="w-56 mx-auto mt-8 sm:w-80 sm:px-0">    
       <Header />
-      <Footer />
+      <Body />
     </div>
     
   );
@@ -23,9 +23,9 @@ export default function Home() {
 export function Header() {
   return (
     <div className="flex flex-wrap space-x-3">
-      <Link href="/talks"><a><img src="/talks.png" className="header" /></a></Link>
-      <Link href="/writing"><a><img src="/writing.png" className="header" onMouseOver={e => e.currentTarget.src = "writing_highlight.png"} onMouseOut={e => e.currentTarget.src = "writing.png"}/></a></Link>
-      <Link href="/projects"><a><img src="/projects_highlight.png" className="header" onMouseOver={e => e.currentTarget.src = "projects_highlight.png"} onMouseOut={e => e.currentTarget.src = "projects.png"}/></a></Link>
+       <Link href="/talks"><a><img src="/talks.png" className="header" onMouseOver={e => e.currentTarget.src = "talks_highlight.png"} onMouseOut={e => e.currentTarget.src = "talks.png"}/></a></Link>
+      <img src="/writing_highlight.png" className="header" />
+      <Link href="/projects"><a><img src="/projects.png" className="header" onMouseOver={e => e.currentTarget.src = "projects_highlight.png"} onMouseOut={e => e.currentTarget.src = "projects.png"}/></a></Link>
       <Link href="/work"><a><img src="/work.png" className="header" onMouseOver={e => e.currentTarget.src = "work_highlight.png"} onMouseOut={e => e.currentTarget.src = "work.png"}/></a></Link>
     </div>
   )
@@ -33,11 +33,11 @@ export function Header() {
 
 
 
-export function Footer() {
+export function Body() {
   return (
     <div className="grid mt-12 place-items-center">
       <div>
-        <img src="/hi.png" />
+        
       </div>
     </div>
     )
