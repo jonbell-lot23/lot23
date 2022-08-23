0330f29d587fa4c1abb16b754ef7ea8396d902d7
Jon Bell <github@lot23.com>
2022-08-23 18:05:57 +1200

    Fixing moble

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+export default function Header() {
+
+    const pages = ["talks", "writing", "projects", "work"];
+    const navitems = pages.map(page => 
+    <div>
+       <Nav page={page} />
+    </div>
+    );
+ 
+    return <div>{navitems}</div>;
+ 
+}
+
+export function Nav({page}) { 
+
+    const link = `/${page}`
+    const img = `/${page}.png`
+    const highlight = `${page}_highlight.png`.toString()
+    
+    console.log(highlight);
+ 
+    return (
+        <Link href={link}><a>
+        <img
+            src={img} 
+            className="header" 
+            layout="fill"
+            onMouseOver={e => e.currentTarget.src = {highlight}}
+            onMouseOut={e => e.currentTarget.src = {img}} />
+        </a></Link>
+    )
+}
--- src/pages/newindex.tsx
+++ src/pages/newindex.tsx
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
-import Link from 'next/link'
+import Header from '../components/Header.tsx'
 
 let socket;
 
@@ -19,19 +19,6 @@ export default function Home() {
     
   );
 }
-
-export function Header() {
-  return (
-    <div className="flex flex-wrap space-x-3">
-      <Link href="/talks"><a><img src="/talks.png" className="header" /></a></Link>
-      <Link href="/writing"><a><img src="/writing.png" className="header" onMouseOver={e => e.currentTarget.src = "writing_highlight.png"} onMouseOut={e => e.currentTarget.src = "writing.png"}/></a></Link>
-      <Link href="/projects"><a><img src="/projects_highlight.png" className="header" onMouseOver={e => e.currentTarget.src = "projects_highlight.png"} onMouseOut={e => e.currentTarget.src = "projects.png"}/></a></Link>
-      <Link href="/work"><a><img src="/work.png" className="header" onMouseOver={e => e.currentTarget.src = "work_highlight.png"} onMouseOut={e => e.currentTarget.src = "work.png"}/></a></Link>
-    </div>
-  )
-}
-
-
 
 export function Footer() {
   return (
