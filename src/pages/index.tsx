b4cb56aa9e93727fa94e9a1d2c0b80db31c1b4d0
Jon Bell <github@lot23.com>
2022-10-02 21:10:45 +1300

    Center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
-import Header from '../components/Header.js'
-import Head from 'next/head'
+import Header from "../components/Header.js";
+import Head from "next/head";
 
 let socket;
 
@@ -12,56 +11,58 @@ type Message = {
 };
 
 export default function Home() {
-
-  return (   
+  return (
     <>
       <Head>
-      <div className="hidden">
-        <img src="/talks.png" />
-        <img src="/talks_highlight.png" />
-        <img src="/writing.png" />
-        <img src="/writing_highlight.png" />
-        <img src="/work.png" />
-        <img src="/work_highlight.png" />
-        <img src="/projects.png" />
-        <img src="/projects_highlight.png" />
+        <div className="hidden">
+          <img src="/talks.png" />
+          <img src="/talks_highlight.png" />
+          <img src="/writing.png" />
+          <img src="/writing_highlight.png" />
+          <img src="/work.png" />
+          <img src="/work_highlight.png" />
+          <img src="/projects.png" />
+          <img src="/projects_highlight.png" />
 
-        <img src="/goodnews.png" />
-        <img src="/abuse.png" />
-        <img src="/greatdesign.png" />
+          <img src="/goodnews.png" />
+          <img src="/abuse.png" />
+          <img src="/greatdesign.png" />
 
-        <img src="/book1.png" />
-        <img src="/book2.png" />
-        <img src="/book3.png" />
-        <img src="/bedtime.png" />
-        <img src="/42and21.png" />
-        <img src="/plebe.png" />          
-      </div>
+          <img src="/book1.png" />
+          <img src="/book2.png" />
+          <img src="/book3.png" />
+          <img src="/bedtime.png" />
+          <img src="/42and21.png" />
+          <img src="/plebe.png" />
+        </div>
       </Head>
-      <div className="mx-auto mt-8 w-80 sm:px-0">  
+      <div className="mx-auto mt-8 w-80 sm:px-0">
         <Header />
         <Body />
       </div>
-    </> 
+    </>
   );
 }
 
 export function Body() {
   return (
     <div className="grid px-6 mt-48 place-items-center">
-      <div className="text-left">
-      <p className="font-semibold">Hello! I'm Jon Bell.</p>
-              <p className="text-md">
-                I'm a product designer that loves to write, code, draw, teach,
-                lead teams, and learn new things.
-              </p>
+      <div className="text-center">
+        <p className="font-semibold">Hello! I'm Jon Bell.</p>
+        <p className="text-md">
+          I'm a product designer that loves to write, code, draw, teach, lead
+          teams, and learn new things.
+        </p>
 
-              <p className="text-md">
-                My email is 
-                 <a href="mailto:jb@lot23.com" className="pl-1">jb@lot23.com</a>. <br />Thanks for
-                stopping by!
-              </p>
+        <p className="text-md">
+          My email is
+          <a href="mailto:jb@lot23.com" className="pl-1">
+            jb@lot23.com
+          </a>
+          .
+        </p>
+        <p>Thanks for stopping by!</p>
       </div>
     </div>
-    )
-  }
+  );
+}
