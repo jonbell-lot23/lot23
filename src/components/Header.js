bda97b786cd7006d18a5bd55dfdeff249e4d0469
Jon Bell <github@lot23.com>
2023-01-04 04:52:26 +0000

    Attempting new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,38 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Header() {
-    const pages = ["talks", "writing", "work", "projects"];
-    const navitems = pages.map(page => 
+  const pages = ["talks", "writing", "work", "projects"];
+  const navitems = pages.map((page) => (
     <div>
-       <Nav page={page} />
+      <Nav page={page} />
     </div>
-    );
-    
-    return (
+  ));
+
+  return (
     <div className="px-4 mx-auto mt-8 w-80">
-        <div className="flex flex-wrap space-x-0">{navitems}</div>
-    </div>); 
+      <div className="flex flex-wrap space-x-0">{navitems}</div>
+    </div>
+  );
 }
 
-export function Nav({page}) { 
+export function Nav({ page }) {
+  const router = useRouter();
+  const currentPath = router.asPath;
+  const link = `/${page}`;
 
-    const router = useRouter()
-    const currentPath = router.asPath;
-    const link = `/${page}`
-    
-    const img = currentPath == link ? `/${page}_highlight.png` : `/${page}.png`
-    const highlight = `${page}_highlight.png`.toString()
+  const img = currentPath == link ? `/${page}_highlight.png` : `/${page}.png`;
+  const highlight = `${page}_highlight.png`.toString();
 
-    return (
-        <Link href={link}><a>
-        <img
-            src={img} 
-            className="header" 
-            onMouseOver={e => e.currentTarget.src = highlight}
-            onMouseOut={e => e.currentTarget.src = img} />
-        </a></Link>
-    )
+  return (
+    <Link href={link}>
+      <img
+        src={img}
+        className="header"
+        onMouseOver={(e) => (e.currentTarget.src = highlight)}
+        onMouseOut={(e) => (e.currentTarget.src = img)}
+      />
+    </Link>
+  );
 }
